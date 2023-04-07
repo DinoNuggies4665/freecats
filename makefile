cadf224ed252b6daab29710a00d55a46fece1e82
DinoNuggies4665 <wwalrathIV@gmail.com>
2023-04-07 16:27:17 -0500

    new file:   data/13.gif 	new file:   data/14.gif 	new file:   data/15.gif 	new file:   data/16.gif 	new file:   data/17.gif 	new file:   data/18.gif 	new file:   data/19.gif 	new file:   data/20.gif 	new file:   data/21.gif 	new file:   data/22.gif 	new file:   data/23.gif 	new file:   data/24.gif 	new file:   data/25.gif 	new file:   data/26.gif 	new file:   data/27.gif 	new file:   data/28.gif 	new file:   data/29.gif 	new file:   data/30.gif 	new file:   data/31.gif 	new file:   data/32.gif 	new file:   data/33.gif 	new file:   data/34.gif 	new file:   data/35.gif 	new file:   data/36.gif 	new file:   data/37.gif 	new file:   data/38.gif 	new file:   data/39.gif 	new file:   data/40.gif 	new file:   data/41.gif 	new file:   data/42.gif 	new file:   data/L.gif 	new file:   data/yippi.ogg 	modified:   makefile 	modified:   src/main.cpp

--- makefile
+++ makefile
@@ -3,11 +3,11 @@ all:
 	mkdir -p build/
 	ccache g++ -c src/main.cpp -o build/main.o
 	ccache g++ -c src/gif.cpp -o build/gif.o
-	ccache g++ build/*.o -lsfml-system -lsfml-window -lsfml-graphics
+	ccache g++ build/*.o -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio
 
 windows:
 	clear
 	mkdir -p build/
 	ccache x86_64-pc-linux-gnu-g++ -c src/main.cpp -o build/main.o
 	ccache x86_64-pc-linux-gnu-g++ -c src/gif.cpp -o build/gif.o
-	ccache x86_64-pc-linux-gnu-g++ build/*.o -o FREECATS.EXE -lsfml-system -lsfml-window -lsfml-graphics
+	ccache x86_64-pc-linux-gnu-g++ build/*.o -o FREECATS.EXE -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "gif.h"
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 #include <stdio.h>
 #include <cstdlib>
@@ -10,67 +11,105 @@
 // Window shit
 std::vector<sf::RenderWindow*> window;
 sf::Event event;
+sf::SoundBuffer buffer;
+std::vector<sf::Sound> sound;
 std::vector<int> xv;
 std::vector<int> yv;
 const int SCREEN_WIDTH = sf::VideoMode().getDesktopMode().width;
 const int SCREEN_HEIGHT = sf::VideoMode().getDesktopMode().height;
 bool isRunning = true;
+int windows;
 
 // Cat shit
 std::vector<AnimatedGIF> gif;
 std::vector<sf::Sprite> sprite;
+std::vector<int> gifID;
+
+void spawnWindow() {
+	// Create path to selected gif
+	std::stringstream _ss;
+	_ss << "data/" << gifID[windows] << ".gif";
+	std::string _path = _ss.str();
+
+	// Load selected gif
+	gif.push_back(AnimatedGIF(_path.c_str()));
+	sprite.push_back(sf::Sprite());
+
+	// Create borderless window
+	window.push_back(new sf::RenderWindow(sf::VideoMode(gif[windows].getSize().x, gif[windows].getSize().y), "АМОГУС", sf::Style::None));
+	window[windows]->setFramerateLimit(60);
+	int _x = rand() % SCREEN_WIDTH;
+	int _y = rand() % SCREEN_HEIGHT;
+	int _xv = rand() % 2 - 1; if (_xv == 0) { _xv++; }
+	int _yv = rand() % 2 - 1; if (_yv == 0) { _yv++; }
+	int _rand = rand() % 9 + 1;
+	_xv *= _rand;
+	_yv *= _rand;
+	xv.push_back(_xv);
+	yv.push_back(_yv);
+	window[windows]->setPosition(sf::Vector2i(_x, _y));
+	sound.push_back(sf::Sound());
+	sound[windows].setBuffer(buffer);
+	float _pitch = rand() % 99 + 1;
+	_pitch /= 100;
+	sound[windows].setPitch(_pitch);
+
+	windows++;
+}
 
 int main() {
 
-	std::vector<int> _gifID;
-	for (int i = 0; i < 13; i++)
-		_gifID.push_back(i);
+	// Randomizing gifs
+	for (int i = 0; i < 42; i++)
+		gifID.push_back(i);
 	srand(time(NULL));
-	std::random_shuffle(_gifID.begin(), _gifID.end());
+	std::random_shuffle(gifID.begin(), gifID.end());
 
-	for (int i = 0; i < 5; i++) {
-		// Create path to selected gif
-		std::stringstream _ss;
-		_ss << "data/" << _gifID[i] << ".gif";
-		std::string _path = _ss.str();
+	// Setting the sound buffer
+	buffer.loadFromFile("data/yippi.ogg");
 
-		// Load selected gif
-		gif.push_back(AnimatedGIF(_path.c_str()));
-		sprite.push_back(sf::Sprite());
+	// The first window
+	spawnWindow();
 
-		// Create borderless window
-		window.push_back(new sf::RenderWindow(sf::VideoMode(gif[i].getSize().x, gif[i].getSize().y), "АМОГУС", sf::Style::None));
-		window[i]->setFramerateLimit(60);
-		int _x = rand() % SCREEN_WIDTH;
-		int _y = rand() % SCREEN_HEIGHT;
-		int _xv = rand() % 2 - 1; if (_xv == 0) { _xv++; }
-		int _yv = rand() % 2 - 1; if (_yv == 0) { _yv++; }
-		int _rand = rand() % 9 + 1;
-		_xv *= _rand;
-		_yv *= _rand;
-		xv.push_back(_xv);
-		yv.push_back(_yv);
-		window[i]->setPosition(sf::Vector2i(_x, _y));
-	}
-
+	int t = 0;
 	while (isRunning) {
+		if (t > 400) {
+			spawnWindow();
+			t = 0;
+		}
+
 		for (int i = 0; i < window.size(); i++) {
 			// Poll events
 			while (window[i]->pollEvent(event)) {
 				if (event.type == sf::Event::KeyPressed) {
-					if (event.key.code == sf::Keyboard::Escape)
+					if (event.key.code == sf::Keyboard::RAlt)
 						isRunning = false;
+					else
+						spawnWindow();
 				}
+				if (event.type == sf::Event::MouseButtonPressed)
+					spawnWindow();
+				if (event.type == sf::Event::MouseButtonReleased)
+					spawnWindow();
 			}
 
 			// Bounce windows
+			bool bounceX = false, bounceY = false;
 			int _x = window[i]->getPosition().x;
 			int _y = window[i]->getPosition().y;
-			if (_x > SCREEN_WIDTH * 2 - window[i]->getSize().x - 1 or _x < 1)
+			if (_x > SCREEN_WIDTH * 2 - window[i]->getSize().x - 1 or _x < 1) {
 				xv[i] *= -1;
-			if (_y > SCREEN_HEIGHT - window[i]->getSize().y - 1 or _y < 31)
+				bounceX = true;
+				sound[i].play();
+			}
+			if (_y > SCREEN_HEIGHT - window[i]->getSize().y - 1 or _y < 31) {
 				yv[i] *= -1;
+				bounceY = true;
+				sound[i].play();
+			}
 			window[i]->setPosition(sf::Vector2i(_x + xv[i], _y + yv[i]));
+			if (bounceX and bounceY)
+				printf("A Window hit the Edge!🥳🥳🥳🥳🎉🎉🎉🎉🎉🎉\n");
 
 			// Draw funny cat
 			gif[i].update(sprite[i]);
@@ -78,6 +117,7 @@ int main() {
 			window[i]->draw(sprite[i]);
 			window[i]->display();
 		}
+		t++;
 	}
 
 	// Memory management shit
